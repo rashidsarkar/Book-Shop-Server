c04948f50eeb354ad3db1abbaca50e4adf06e948
rashidsarker <rashidrock558@gmail.com>
2024-11-21 21:58:18 +0600

    feat: add TProduct type definition for product structure

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,12 @@ import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 
 const app: Application = express();
-const port = 3000;
+
 // parsers
 app.use(express.json());
 app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+  res.send('Hello World from book!');
 });
 export default app;
---
+++ src/app/modules/product/product.interface.ts
@@ -0,0 +1,9 @@
+export type TProduct = {
+  title: string;
+  author: string;
+  price: number;
+  category: 'Fiction' | 'Science' | 'SelfDevelopment' | 'Poetry' | 'Religious';
+  description: string;
+  quantity: number;
+  inStock: boolean;
+};
