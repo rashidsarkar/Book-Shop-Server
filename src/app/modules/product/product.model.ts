4a308a056bcf612645199ace12cd680e481213c4
rashidsarker <rashidrock558@gmail.com>
2024-11-21 22:09:18 +0600

    feat: create Mongoose product schema with TProduct interface integration

---
+++ src/app/modules/product/product.model.ts
@@ -0,0 +1,38 @@
+import { model, Schema } from 'mongoose';
+import { TProduct } from './product.interface';
+
+const productSchema = new Schema<TProduct>({
+  title: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+    enum: {
+      values: ['Fiction', 'Science', 'SelfDevelopment', 'Poetry', 'Religious'],
+      message: 'Invalid category : {VALUE}',
+    },
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  inStock: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+export const ProductModel = model<TProduct>('Product', productSchema);
